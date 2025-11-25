7d8af65347c4be08e6e2a126330cdc7844bad448
Vinaykumar1809 <vinaykchaudhary2006@gmail.com>
2025-11-25 11:52:55 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,9 +6,6 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true, // ALLOWS sending cookies for auth
-  headers: {
-    'Content-Type': 'application/json'
-  }
 });
 
 // Add a request interceptor to add the token each time
